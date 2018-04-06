cb09493d8ba85b60191d861317ba773da1912fce
Abdul2010 <abdu0445@edu.easj.dk>
2018-04-06 06:55:07 +0200

    Can show two cvs files at the same time

--- Csv.js
+++ Csv.js
@@ -51,3 +51,61 @@ function drawOutput(lines){
     }
     document.getElementById("output").appendChild(table);
 }
+
+
+//----------------------------------------------------------------------------------------------------------------------
+//csv 2
+
+function handleFiles2(files) {
+    // Check for the various File API support.
+    if (window.FileReader) {
+        // FileReader are supported.
+        getAsText2(files[0]);
+    } else {
+        alert('FileReader are not supported in this browser.');
+    }
+}
+
+function getAsText2(fileToRead2) {
+    var reader2 = new FileReader();
+    // Handle errors load
+    reader2.onload = loadHandler2;
+    reader2.onerror = errorHandler2;
+    // Read file into memory as UTF-8
+    reader2.readAsText(fileToRead2);
+}
+
+function loadHandler2(event) {
+    var csv2 = event.target.result;
+    processData2(csv2);
+}
+
+function processData2(csv2) {
+    var allTextLines = csv2.split(/\r\n|\n/);
+    var lines2 = [];
+    while (allTextLines.length) {
+        lines2.push(allTextLines.shift().split());
+    }
+    console.log(lines2);
+    drawOutput2(lines2);
+}
+
+function errorHandler2(evt) {
+    if(evt.target.error.name == "NotReadableError") {
+        alert("Canno't read file !");
+    }
+}
+
+function drawOutput2(lines2){
+    //Clear previous data
+    document.getElementById("output2").innerHTML = "";
+    var table2= document.createElement("table");
+    for (var i = 0; i < lines2.length; i++) {
+        var row2 = table2.insertRow(-1);
+        for (var j = 0; j < lines2[i].length; j++) {
+            var firstNameCell2 = row2.insertCell(-1);
+            firstNameCell2.appendChild(document.createTextNode(lines2[i][j]));
+        }
+    }
+    document.getElementById("output2").appendChild(table2);
+}
